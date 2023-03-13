80e338b4f0be1a3116fa95401581fb84ec5a5ffa
Van Phat <vanphathutechit@gmail.com>
2023-03-13 20:20:33 +0700

    implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ExceptionsFilter } from './util/exception.filter';
 
@@ -14,6 +15,15 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ forbidUnknownValues: false, transform: true }),
   );
+
+  // Setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('CRUD Interview')
+    .setDescription('The project for CRUD interview')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
 
   // Run server
   await app.listen(5000);
